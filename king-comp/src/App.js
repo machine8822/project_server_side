c1ae45161afaecd69c601d4693cd66dfbbb3593c
Gregor Cool <willytk249@gmail.com>
2025-03-25 09:07:36 -0400

    class changes

--- king-comp/src/App.js
+++ king-comp/src/App.js
@@ -1,10 +1,20 @@
 import './App.css';
+import Tree from './components/Tree';
 
 function App() {
   return (
-    <div>
-      <h1>Hi There.</h1>
-    </div>
+    <>
+    <section className="columns">
+        <Tree 
+        name="Live Oak"
+        description="Doesn't lose it's leaves"
+        image="./images/liveoak.jpg"/>
+        <Tree 
+        name="Dogwood"
+        description="Flowers in spring"
+        image="./images/dogwood.jpg"/>
+      </section>
+    </>
   );
 }
 
