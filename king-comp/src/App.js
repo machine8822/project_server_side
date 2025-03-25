6925c8792c470c5f595a751df4e26e78ebfed0da
Gregor Cool <willytk249@gmail.com>
2025-03-25 09:16:19 -0400

    class changes

--- king-comp/src/App.js
+++ king-comp/src/App.js
@@ -1,19 +1,21 @@
 import './App.css';
 import Tree from './components/Tree';
+import Header from './components/Header'
 
 function App() {
   return (
     <>
-    <section className="columns">
-        <Tree 
-        name="Live Oak"
-        description="Doesn't lose it's leaves"
-        image="./images/liveoak.jpg"/>
-        <Tree 
-        name="Dogwood"
-        description="Flowers in spring"
-        image="./images/dogwood.jpg"/>
-      </section>
+      <Header />
+      <section className="columns">
+          <Tree 
+          name="Live Oak"
+          description="Doesn't lose it's leaves"
+          image="./images/liveoak.jpg"/>
+          <Tree 
+          name="Dogwood"
+          description="Flowers in spring"
+          image="./images/dogwood.jpg"/>
+        </section>
     </>
   );
 }
---
+++ king-comp/src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import "./css/Footer.css";
+
+function Footer() {
+    return(
+        <footer>
+            <h1>Foot of the Site</h1>
+        </footer>
+    );
+    
+};
+
+export default Header;
