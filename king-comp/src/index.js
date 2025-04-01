6cc781d2b2ed1374ab0f70760c46d3393a9306f3
King <willytk249@gmail.com>
2025-03-31 21:50:31 -0400

    deploy testing

--- king-comp/src/index.js
+++ king-comp/src/index.js
@@ -12,7 +12,7 @@ import Checkout from './pages/Checkout';
 
 function App() {
   return(
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
